849c65588af775cfc7af70ed32c3e0babe11b011
Brandon Chung <bachung@Brandons-MacBook-Pro.local>
2017-01-10 22:47:33 -0500

    Added handlers for tick, pause, and seek

--- players/VLC/VLCManager.js
+++ players/VLC/VLCManager.js
@@ -2,7 +2,7 @@ const spawn = require("child_process").spawn;
 
 const DEFAULT_ARGS = ["--extraintf", "rc"];
 
-
+const exports = module.exports;
 
 /**
  * Creates an instance of a Manager to manage VLC
@@ -45,6 +45,7 @@ Manager.prototype.start = function () {
   });
 
 
+
   this.listener = setInterval(function() {
     if (manager.process != undefined) {
       manager.process.stdin.write("status\n");
@@ -53,16 +54,34 @@ Manager.prototype.start = function () {
   }, 100);
 };
 
+
+/**
+ * anonymous function - description
+ *
+ * @param  {type} command description
+ * @return {type}         description
+ */
 Manager.prototype.processCommand = function (command) {
   let time = /^\d+/;
   let state = /^\( state (.+) \)/;
   if (time.test(command)) {
     let t = parseInt(command);
+    if (Math.abs(t - this.currentTime) > 1) {
+      this.onSeek(t);
+    }
     this.currentTime = t;
+    if (t != this.currentTime) {
+      if (this.tickHandler !== undefined) {
+        this.tickHandler(t);
+      }
+    }
   } else if (state.test(command)) {
     let paused = command.match(state)[1] != "playing";
     if (paused != this.paused) {
       this.paused = paused;
+      if (this.onPauseToggle !== undefined) {
+        this.pauseHandler(paused);
+      }
     }
   }
 };
@@ -82,6 +101,11 @@ Manager.prototype.onFileLoad = function (handler) {
   return this;
 };
 
+Manager.prototype.onTick = function (handler) {
+  this.tickHandler = handler;
+  return this;
+}
+
 Manager.prototype.pause = function () {
   if (!this.paused) {
     this.togglePause();
@@ -94,7 +118,14 @@ Manager.prototype.resume = function () {
   }
 };
 
-Manager.prototype.togglePause = function() {
+Manager.prototype.togglePause = function () {
   this.paused = !this.paused;
   this.process.stdin.write("pause\n");
 };
+
+Manager.prototype.seek = function (time) {
+  this.currentTime = time;
+  this.process.stdin.write("seek " + time + "\n");
+};
+
+exports.VLC = Manager;
