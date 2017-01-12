172d60963c758e0a03403f2f19702e71ed0c8a4f
Brandon Chung <bachung@Brandons-MacBook-Pro.local>
2017-01-11 20:08:55 -0500

    Did a thing that didn't work

--- controller/Controller.js
+++ controller/Controller.js
@@ -7,7 +7,7 @@ const path = "/Applications/VLC.app/Contents/MacOS/VLC";
 const host = "gablescode.net";
 const port = 12345;
 
-const name = "bachung4";
+const name = "bachung6";
 const room = "test";
 
 const playerFactory = require('../players/PlayerFactory');
@@ -28,22 +28,39 @@ function onSeekMessage(time) {
   player.seek(time);
 }
 
+function onInfoMessage(sender) {
+  netManager.sendTo(sender, "info " + (player.paused ? "paused" : "playing")
+    + " " + player.getTime());
+}
+
 
 netManager.connect(name, room).then(function () {
+  let receivedInfo = false;
   netManager.onData(function (sender, data) {
     let pauseRegex = /^(paused|playing) (\d+)/;
     let seekRegex = /^seek (\d+)/;
+    let infoRegex = /^info/;
+    let infoResponseRegex = /^info (paused|playing) (\d+)/;
     if (pauseRegex.test(data)) {
       let e = data.match(pauseRegex);
       onPauseMessage(e[1] == "paused", parseInt(e[2]));
     } else if (seekRegex.test(data)) {
       let e = data.match(seekRegex);
       onSeekMessage(parseInt(e[1]));
+    } else if (infoRegex.test(data)) {
+      onInfoMessage(sender);
+    } else if (infoResponseRegex.test(sender)) {
+      if (!receivedInfo) {
+        let e = data.match(infoResponseRegex);
+        onPauseMessage(e[1] == "paused", parseInt(e[2]));
+        receivedInfo = true;
+      }
     }
   });
   netManager.onDisconnect(function () {
 
   });
+  netManager.broadcast("info");
 });
 
 player.onPauseToggle(function (paused) {
